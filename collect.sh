47f278b7665fef63818b7bced4e21be4ff1f816c
jdh <dongho971220@gmail.com>
2022-05-11 07:17:52 +0900

    update

--- collect.sh
+++ collect.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 entries=(
-	dotfiles:"$HOME/.zshrc"
-	dotfiles:"$HOME/.vimrc"
-	dotfiles:"$HOME/.gitconfig"
-	configs:"$HOME/.config/i3/config":i3
-	configs:"$HOME/.config/i3status/config":i3status
-	configs:"$HOME/.config/i3status/config":flameshot
+    dotfiles:"$HOME/.zshrc":zshrc
+    dotfiles:"$HOME/.vimrc":vimrc
+    dotfiles:"$HOME/.gitconfig":gitconfig
+    configs:"$HOME/.config/i3/config":i3
+    configs:"$HOME/.config/i3status/config":i3status
+    configs:"$HOME/.config/flameshot/flameshot.ini":flameshot
+    spool:"/var/spool/cron/$(whoami)":cron  # might need `chmod o+rx /var/spool/cron`
 )
 
 for entry in "${entries[@]}"
 do
-	IFS=: read category path name <<< $entry
-
-	if [ -z "$name" ]
-	then
-		ln -f $path ./${category}
-	else
-		ln -f $path ./${category}/${name}
-	fi	
+    IFS=: read category path name <<< $entry
+    cp $path ./${category}/${name}
 done
