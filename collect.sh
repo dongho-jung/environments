2c8ce73b95de4716c5ca81ffa9c2e6cc1ee075b6
jdh <dongho971220@gmail.com>
2022-05-18 13:57:09 +0900

    add directory like *.d

--- collect.sh
+++ collect.sh
@@ -8,12 +8,14 @@ entries=(
     configs:"$HOME/.config/i3status/config":i3status
     configs:"$HOME/.config/flameshot/flameshot.ini":flameshot
     spool:"/var/spool/cron/$(whoami)":cron  # might need `chmod o+rx /var/spool/cron`
+    directory:"$HOME/.uim.d":uim
 )
 
 for entry in "${entries[@]}"
 do
     IFS=: read category path name <<< $entry
-    cp $path ./${category}/${name}
+    mkdir -p ./$category
+    cp -r $path ./$category/$name
 done
 
 git status
