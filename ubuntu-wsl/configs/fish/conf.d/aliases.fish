e077914b28ff9aa80e7a62f5f60b1772bee1b553
dongho-jung <dongho971220@gmail.com>
2024-01-20 19:44:03 +0900

    add ubuntu-wsl

---
+++ ubuntu-wsl/configs/fish/conf.d/aliases.fish
@@ -0,0 +1,23 @@
+alias l ls
+
+alias ga 'git add'
+alias gc 'git commit -v'
+alias gl 'git pull'
+alias gp 'git push'
+alias gg 'git gui'
+alias gst 'git status'
+alias gd 'git diff'
+alias gds 'git diff --staged'
+alias gco 'git checkout'
+
+alias tf terraform
+alias tfp 'tf plan'
+alias tfa 'tf apply'
+alias tfi 'tf init'
+
+alias wy clip.exe
+alias wp 'powershell.exe -noprofile -command Get-Clipboard'
+alias y 'xclip -selection clipboard'
+alias p 'xclip -selection clipboard -o'
+
+alias k kubectl
