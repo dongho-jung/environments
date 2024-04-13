ca16a77b0a368f75fa1407c06604debacfe78696
dongho-jung <dongho971220@gmail.com>
2024-04-13 14:43:47 +0900

    WIP

---
+++ nixos-desktop/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.dongho-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
+
