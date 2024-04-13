22bb2996b7e167a89699b59c8ca26e3fd2b8fc4a
dongho-jung <dongho971220@gmail.com>
2024-04-13 14:43:47 +0900

    WIP

--- nixos-desktop/configuration.nix
+++ nixos-desktop/configuration.nix
@@ -1,42 +1,55 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
-
 let
-  nur-no-pkgs = import (builtins.fetchTarball
-    "https://github.com/nix-community/NUR/archive/master.tar.gz"
-  ) {};
+  nur-no-pkgs = import
+    (
+      builtins.fetchTarball {
+        url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
+        # sha256 = "12wi8gkq6cyfwb86hk4z8rn55wp83c1ql3zp1jdph9mmw7fjfl2c";
+      }
+    )
+    { };
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
-in {
+in
+{
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       nur-no-pkgs.repos.LuisChDev.modules.nordvpn
     ];
-  nix.settings.experimental-features = ["nix-command"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      defaultSession = "none+i3";
+      sessionCommands = ''
+        ${pkgs.xorg.xmodmap}/bin/xmodmap -e 'keycode 66 = Hangul' &
+      '';
+    };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        i3lock
+        i3status
+      ];
+    };
+    xkbOptions = "caps:none";
+    videoDrivers = ["nvidia"];
+  };
 
-
-  # Use the systemd-boot EFI boot loader.
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernel.sysctl."kernel.sysrq" = 1;
   };
 
-  networking.hostName = "dongho-nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "dongho-nixos";
 
-  # Set your time zone.
   time.timeZone = "Asia/Seoul";
-
 
   i18n = {
     inputMethod = {
-      enabled = "kime";
+      enabled = "uim";
     };
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
@@ -54,45 +67,27 @@ in {
     ];
     fontconfig = {
       defaultFonts = {
-        serif = ["Noto Sans Mono CJK KR"];
-        sansSerif = ["Noto Sans Mono CJK KR"];
-        monospace = ["Noto Sans Mono CJK KR"];
-        # serif = ["NanumGothicCoding"];
-        # sansSerif = ["NanumGothicCoding"];
-        # monospace = ["NanumGothicCoding"];
+        serif = [ "Noto Sans Mono CJK KR" ];
+        sansSerif = [ "Noto Sans Mono CJK KR" ];
+        monospace = [ "Noto Sans Mono CJK KR" ];
       };
     };
   };
-
-  # Enable the X11 windowing system.
-  #  services.xserver = {
-  #    enable = true;
-  #    desktopManager = { xterm.enable = false; };
-  #    displayManager = {
-  #      defaultSession = "none+i3";
-  #      sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap -e 'keycode 66 = Hangul' &";
-  #    };
-  #    windowManager.i3 = {
-  #      enable = true;
-  #      extraPackages = with pkgs; [
-  #        dmenu
-  #        i3status
-  #        i3lock
-  #        i3blocks
-  #      ];
-  #    };
-  #    xkbOptions = "caps:none";
-  #    videoDrivers = ["nvidia"];
-  #  };
 
   nixpkgs.config = {
     allowUnfree = true;
   };
 
   nixpkgs.config.packageOverrides = pkgs: rec {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
+    nur = import
+      (
+        builtins.fetchTarball {
+          url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
+          # sha256 = "12wi8gkq6cyfwb86hk4z8rn55wp83c1ql3zp1jdph9mmw7fjfl2c";
+        })
+      {
+        inherit pkgs;
+      };
     nordvpn = nur.repos.LuisChDev.nordvpn;
   };
 
@@ -101,72 +96,32 @@ in {
     allowedTCPPorts = [ 443 ];
     allowedUDPPorts = [ 1194 ];
   };
-  
+
   services.pipewire.enable = true;
   services.nordvpn.enable = true;
-  services.fcron = {
-    enable = true;
-    allow = [ "all" ];
-    systab = ''
-      @mail(no),nolog(true) 30s export export XDG_RUNTIME_DIR=/run/user/1000; ${unstable.swww}/bin/swww img --resize fit --transition-type random --transition-duration 1 $(find /home/dongho/images/wallpapers/ -type f | shuf -n1)
-    '';
-  };
 
-  services.keyd = {
-    enable = true;
-    keyboards.default.settings = {
-      main = {
-        capslock = "hangeul";
-      };
-    };
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = rec {
-      initial_session = {
-        command = "Hyprland";
-	user = "dongho";
-      };
-      default_session = initial_session;
-    };
-  };
-
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
   hardware = {
     opengl.enable = true;
     nvidia.modesetting.enable = true;
-    nvidia.open = true;  # https://askubuntu.com/q/1420777, nouveau error spam
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = false;
 
   programs = {
     fish = {
       enable = true;
     };
-    hyprland = {
-      enable = true;
-      enableNvidiaPatches = true;
-      xwayland.enable = true;
-    };
     autojump.enable = true;
     command-not-found.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dongho = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "nordvpn" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "nordvpn" ];
     shell = pkgs.fish;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     atop
     awscli2
@@ -181,7 +136,6 @@ in {
     docker
     dunst
     eza
-    eww
     fcron
     fd
     feh
@@ -191,7 +145,7 @@ in {
     gcc
     git
     gnome.gnome-terminal
-    google-chrome 
+    google-chrome
     gtk4
     gtk4-layer-shell
     gtkmm4
@@ -212,17 +166,16 @@ in {
     neofetch
     neovim
     nix-index
+    nixpkgs-fmt
+    pamixer
     peek
     pipewire
     powertop
     python3
     python311Packages.ipython
-    python311Packages.jupyter-core
     ripgrep
-    rofi-wayland
+    rofi
     scrot
-    slack
-    unstable.swww
     terraform
     tldr
     trash-cli
@@ -230,29 +183,23 @@ in {
     unstable.alacritty
     virtualenv
     vscode
-    waybar
-    wget 
-    wl-clipboard
-    xdg-desktop-portal-hyprland
+    wget
+    xclip
+    xdotool
     xfce.tumbler
     xorg.xev
     xorg.xmodmap
     xfce.thunar
-
-    (pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    }))
   ];
-  
+
   environment.pathsToLink = [ "/libexec" ];
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = rec {
+    PATH = [
+      "$HOME/projects/scripts/scripts"
+    ];
   };
 
   system.copySystemConfiguration = true;
-
-  system.stateVersion = "23.11"; # Did you read the comment?
-
+  system.stateVersion = "23.11"; 
 }
 
