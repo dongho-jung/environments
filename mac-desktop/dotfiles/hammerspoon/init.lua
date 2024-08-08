4cacbcc277e6b32c2ccbadc445b3fb10687091cc
dongho-jung <dongho971220@gmail.com>
2024-08-08 11:48:46 +0900

    add sleep hotkey

--- mac-desktop/dotfiles/hammerspoon/init.lua
+++ mac-desktop/dotfiles/hammerspoon/init.lua
@@ -1,6 +1,9 @@
 require('modules.terminal')
 require('modules.input-switch')
 require('modules.tiling')
+require('modules.misc-hotkeys')
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
+
+hs.alert.show('Hammerspoon Started...')
