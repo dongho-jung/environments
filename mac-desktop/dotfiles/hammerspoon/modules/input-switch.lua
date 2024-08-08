a03f42049aa377b034424566469381845a1d3eab
dongho-jung <dongho971220@gmail.com>
2024-08-08 11:56:09 +0900

    do not register hotkey dynamically

--- mac-desktop/dotfiles/hammerspoon/modules/input-switch.lua
+++ mac-desktop/dotfiles/hammerspoon/modules/input-switch.lua
@@ -15,13 +15,11 @@ function temp_eng()
   if hs.keycodes.currentSourceID() == "com.apple.keylayout.ABC" then
     if prev_input_source ~= nil then
       hs.keycodes.currentSourceID(prev_input_source)
-      temp_hotkey:delete()
     end
     return
   end
   prev_input_source = hs.keycodes.currentSourceID()
   hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
-  temp_hotkey = hs.hotkey.bind({}, 'return', temp_eng)
 end
 
 hs.hotkey.bind({}, 'f19', temp_eng)
