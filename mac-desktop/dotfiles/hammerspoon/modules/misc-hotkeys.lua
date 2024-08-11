630d1f5a28f2b5b657e2c134570299c02d89eccd
dongho-jung <dongho971220@gmail.com>
2024-08-12 02:57:34 +0900

    need some delay due to logitech feedback issue

--- mac-desktop/dotfiles/hammerspoon/modules/misc-hotkeys.lua
+++ mac-desktop/dotfiles/hammerspoon/modules/misc-hotkeys.lua
@@ -1,1 +1,7 @@
-hs.hotkey.bind({}, 'f6', function() hs.caffeinate.systemSleep() end)
+hs.hotkey.bind({}, 'f6',
+    function()
+        hs.timer.doAfter(1, function()
+            hs.caffeinate.systemSleep()
+        end)
+    end
+)
