6fd90919f506c86e43852f95b09c7c8c8c51c87e
dongho-jung <dongho971220@gmail.com>
2025-12-18 15:48:11 +0900

    feat: add hammerspoon remap module
    - Hammerspoon 키 리매핑 모듈 추가
      - init.lua에서 remap 모듈 로드
      - 새 remap.lua 모듈 파일 추가

--- mac-desktop/hammerspoon/init.lua
+++ mac-desktop/hammerspoon/init.lua
@@ -1,6 +1,7 @@
 require('modules.input-switch')
 require('modules.tiling')
 require('modules.pad')
+require('modules.remap')
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
