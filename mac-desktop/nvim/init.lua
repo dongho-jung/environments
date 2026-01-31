0df5695a7f13e8b527c2330a1c5d9a87e9440bdc
dongho-jung <dongho971220@gmail.com>
2026-01-31 18:13:35 +0900

    refactor!: overhaul neovim config with hardtime plugin
    - 플러그인 변경
      - copilot.vim 제거
      - hardtime.nvim 추가 (vim 습관 개선용)
    - UI 개선
      - relative number 추가 (insert mode에서 자동 토글)
      - statusline에 showcmd 표시 추가
    - 키맵핑 정리
      - 커스텀 라인/워드 네비게이션 키맵 제거 (C-a, C-e, M-b, M-f 등)
      - Ctrl+Up/Down, Alt+Up/Down 키맵 제거
    
    BREAKING CHANGE: copilot 플러그인 및 커스텀 키맵핑 제거됨

--- mac-desktop/nvim/init.lua
+++ mac-desktop/nvim/init.lua
@@ -2,69 +2,77 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
-Plug('github/copilot.vim')
-Plug('ericbn/vim-solarized')
-Plug('mhinz/vim-startify')
-Plug('keaising/im-select.nvim')
+Plug 'ericbn/vim-solarized'
+Plug 'mhinz/vim-startify'
+Plug 'keaising/im-select.nvim'
+Plug 'm4xshen/hardtime.nvim'
 vim.call('plug#end')
 
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = true })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true })
-vim.keymap.set("n", "ç", function()
-  vim.cmd('normal! gg"+yG')
-end, { noremap = true, silent = true })
-
 vim.cmd('syntax enable')
-vim.opt.background = 'light'
 vim.cmd('colorscheme solarized')
 
+vim.opt.background = 'light'
 vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.mouse = 'a'
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
-
-vim.g.copilot_filetypes = {
-  markdown = false,
-  python = true,
-}
-vim.keymap.set("i", "<C-,>", "<Plug>(copilot-previous)", { silent = true, desc = "Copilot Previous" })
-vim.keymap.set("i", "<C-.>", "<Plug>(copilot-next)", { silent = true, desc = "Copilot Next" })
+vim.opt.showcmd = true
+vim.opt.showcmdloc = "statusline"
+vim.opt.statusline = "%S %f %=%l:%c"
 
 require('im_select').setup {}
+require('hardtime').setup {
+    disabled_keys = {
+        ["<Up>"]    = false,
+        ["<Down>"]  = false,
+        ["<Left>"]  = false,
+        ["<Right>"] = false,
+    },
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.fn.system("macism com.apple.keylayout.ABC")
-  end,
+    restricted_keys = {
+        ["<Up>"]    = { "n", "x" },
+        ["<Down>"]  = { "n", "x" },
+        ["<Left>"]  = { "n", "x" },
+        ["<Right>"] = { "n", "x" },
+    },
+}
+
+local augroup = vim.api.nvim_create_augroup("RelNumToggle", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	group = augroup,
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
 })
 
-local opts = { noremap = true, silent = true }
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = augroup,
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
 
--- Normal mode: line / word navigation
-vim.keymap.set('n', '<C-a>', '^', opts)
-vim.keymap.set('n', '<C-e>', '$', opts)
-vim.keymap.set('n', '<M-b>', 'b', opts)
-vim.keymap.set('n', '<M-f>', 'w', opts)
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.fn.system("macism com.apple.keylayout.ABC")
+	end,
+})
 
--- Insert mode: line / word navigation
-vim.keymap.set('i', '<C-a>', '<C-o>^', opts)
-vim.keymap.set('i', '<C-e>', '<C-o>$', opts)
-vim.keymap.set('i', '<M-b>', '<C-o>b', opts)
-vim.keymap.set('i', '<M-f>', '<C-o>w', opts)
+vim.api.nvim_create_autocmd("InsertEnter", {
+	group = augroup,
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
 
--- Ctrl + Up/Down → move to top/bottom of screen
-vim.keymap.set('n', '<C-Up>', 'H', opts)
-vim.keymap.set('n', '<C-Down>', 'L', opts)
-vim.keymap.set('i', '<C-Up>', '<C-o>H', opts)
-vim.keymap.set('i', '<C-Down>', '<C-o>L', opts)
-
--- Alt + Up/Down → jump to previous/next blank line
-vim.keymap.set('n', '<M-Up>', '{', opts)
-vim.keymap.set('n', '<M-Down>', '}', opts)
-vim.keymap.set('i', '<M-Up>', '<C-o>{', opts)
-vim.keymap.set('i', '<M-Down>', '<C-o>}', opts)
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = augroup,
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
