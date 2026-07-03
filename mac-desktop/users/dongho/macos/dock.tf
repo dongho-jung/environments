7bf9d884cab5caffe2edfb392bfca041b71dced0
dongho-jung <dongho971220@gmail.com>
2026-07-02 20:24:42 -0700

    refactor: consolidate macos defaults
    - Replace individual macOS default resources with one grouped resource
    - Keep Dock app and folder configuration separate from defaults
    - Preserve the existing preference values in the consolidated map

--- mac-desktop/users/dongho/macos/dock.tf
+++ mac-desktop/users/dongho/macos/dock.tf
@@ -8,33 +8,3 @@ resource "host_macos_dock" "default" {
     "/Users/dongho/Downloads",
   ]
 }
-
-resource "host_macos_default" "dock_autohide" {
-  domain = "com.apple.dock"
-  key    = "autohide"
-  bool   = true
-}
-
-resource "host_macos_default" "dock_show_recents" {
-  domain = "com.apple.dock"
-  key    = "show-recents"
-  bool   = false
-}
-
-resource "host_macos_default" "dock_minimize_to_application" {
-  domain = "com.apple.dock"
-  key    = "minimize-to-application"
-  bool   = false
-}
-
-resource "host_macos_default" "dock_show_process_indicators" {
-  domain = "com.apple.dock"
-  key    = "show-process-indicators"
-  bool   = true
-}
-
-resource "host_macos_default" "dock_bottom_right_hot_corner" {
-  domain = "com.apple.dock"
-  key    = "wvous-br-corner"
-  int    = 14
-}
