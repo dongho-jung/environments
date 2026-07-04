73f74205f984b202317d0639fddbece0f30be323
dongho-jung <dongho971220@gmail.com>
2026-07-04 09:36:04 -0700

    refactor: use dock item resources
    - Move Dock apps and folders to item-level resources.
    
    - Require host provider v0.9.0 and update the lock file.

--- mac-desktop/users/dongho/packages/google-chrome.tf
+++ mac-desktop/users/dongho/packages/google-chrome.tf
@@ -2,3 +2,8 @@ resource "host_package_brew" "google_chrome" {
   name         = "google-chrome"
   package_type = "cask"
 }
+
+resource "host_mac_dock_app" "google_chrome" {
+  path     = host_package_brew.google_chrome.app_path
+  priority = 20
+}
