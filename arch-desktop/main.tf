685bc1e48c673a90323d1967cc6ddc4e51dc5e38
dongho-jung <dongho971220@gmail.com>
2026-07-31 19:03:08 +0900

    chore(aur): clean build artifacts
    - AUR 빌드 후 불필요한 파일 정리
      - 임시 make dependency를 설치 완료 뒤 제거
      - helper 빌드 캐시를 성공한 작업 뒤 정리

--- arch-desktop/main.tf
+++ arch-desktop/main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "host" {
-  target_user = "dongho"
-  aur_helper  = "yay"
+  target_user                  = "dongho"
+  aur_helper                   = "yay"
+  aur_remove_make_dependencies = true
+  aur_clean_after              = true
 }
