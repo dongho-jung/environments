2c7641675080b5a13184a900722ccbdfbaffd135
dongho-jung <dongho971220@gmail.com>
2026-08-12 18:07:24 +0900

    feat: disable Starship AWS module
    - Starship 설정 관리
      - AWS 프롬프트 모듈을 비활성화하도록 구성

--- arch-desktop/starship.tf
+++ arch-desktop/starship.tf
@@ -2,6 +2,15 @@ resource "host_package_pacman" "starship" {
   name = "starship"
 }
 
+resource "host_file" "starship_config" {
+  path = "~/.config/starship.toml"
+
+  content = <<-EOT
+    [aws]
+    disabled = true
+  EOT
+}
+
 resource "host_file_block" "starship_init" {
   block   = host_file.zshrc.blocks.init
   content = "eval \"$(starship init zsh)\""
