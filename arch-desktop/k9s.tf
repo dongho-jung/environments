aa2b0a46da382840e3843331878b9d7731d606f1
dongho-jung <dongho971220@gmail.com>
2026-09-04 21:50:23 +0900

    feat: theme k9s with Solarized Light
    - K9s 테마 구성 추가
      - v0.51.0 공식 Solarized Light skin을 배포
      - K9S_SKIN 환경 변수로 기본 테마 선택

--- arch-desktop/k9s.tf
+++ arch-desktop/k9s.tf
@@ -1,3 +1,21 @@
 resource "host_package_pacman" "k9s" {
   name = "k9s"
 }
+
+resource "host_file" "k9s_solarized_light_skin" {
+  path    = "~/.config/k9s/skins/solarized-light.yaml"
+  content = file("${path.module}/k9s/skins/solarized-light.yaml")
+
+  depends_on = [
+    host_package_pacman.k9s,
+  ]
+}
+
+resource "host_file_block" "k9s_skin_environment" {
+  block   = host_file.zshrc.blocks.environment
+  content = "export K9S_SKIN=solarized-light"
+
+  depends_on = [
+    host_file.k9s_solarized_light_skin,
+  ]
+}
