d9c01e5a7f3422053f9847726c960a126b05bf61
dongho-jung <dongho971220@gmail.com>
2026-09-26 00:31:25 +0900

    feat: declare the Hidden Bar login item
    - Hidden Bar를 나머지 세 앱과 같은 방식으로 로그인 항목에 선언
      - 지금까지 앱 자체 설정으로만 켜져 있어 새 맥에서는 재현되지 않았음

--- mac-desktop/hiddenbar.tf
+++ mac-desktop/hiddenbar.tf
@@ -2,3 +2,7 @@ resource "host_package_brew" "hiddenbar" {
   name         = "hiddenbar"
   package_type = "cask"
 }
+
+resource "host_mac_login_item" "hiddenbar" {
+  path = host_package_brew.hiddenbar.app_path
+}
